f4b3490bdbc37dd746e0146434ba8720918bff24
inescss <ines.abdouli18@gmail.com>
2022-07-03 15:23:46 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-char a;
-for (a - 'a'; a <- 'z' ;a++)
-putchar(a);
-putchar('\n');
+int ch;
+for ( ch = 97; ch <= 122; ch++ )
+{
+putchar(ch);   
+}
+putchar(10);
 return(0);
 }
