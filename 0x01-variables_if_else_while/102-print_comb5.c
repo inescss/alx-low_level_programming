47b961873253112f614667c863d109418b6ae280
inescss <ines.abdouli18@gmail.com>
2022-07-04 07:27:57 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * return: alway  0
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48 ; ch <= 57 ; ch++)
+{
+for (n = 48 ;  n <= 57 ; n++)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
+
