c737d09005e5f8d2102495dab443ce2a7da9191a
inescss <ines.abdouli18@gmail.com>
2022-07-04 12:08:13 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * return: always  0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z' ; ch <= 'a' ; ch--)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
+
