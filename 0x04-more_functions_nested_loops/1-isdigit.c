06bf17a8ec7a9b92ea4ba5fa521b8e23139e8cc2
inescss <ines.abdouli18@gmail.com>
2022-07-08 13:59:13 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  -isupper -> this is a function to check upper case alphas
+ *  @c: an integer paramer
+ *  Return: something but success
+ */
+int _isdigit(int c)
+{
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
+}
