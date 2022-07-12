d748724b26ca1a391fe14093ee7ca5d418feb571
inescss <ines.abdouli18@gmail.com>
2022-07-12 11:41:09 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * _strcpy - copiar el contendio de uno a otra variable
+ * @dest: this is destiny
+ * @src: this is the copia
+ *
+ * Return: this return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *start = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (start);
+}
